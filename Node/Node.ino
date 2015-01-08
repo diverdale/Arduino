25abff87307e33364835470b5b35cf6baa6bc619
Dale Wright <diverdale@gmail.com>
2015-01-07 19:50:05 -0500

    Streamlined code...removing unnecessary bits. Added powerDown and radio.sleep to save battery

--- Node/Node.ino
+++ Node/Node.ino
@@ -1,31 +1,38 @@
-// Sample RFM69 sender/node sketch, with ACK and optional encryption
-// Sends periodic messages of increasing length to gateway (id=1)
-// It also looks for an onboard FLASH chip, if present
-// Library and code by Felix Rusu - felix@lowpowerlab.com
+/*Generic Sensor Node Sketch
+//Jan 2015 -- Dale Wright
+//
+*/
 
-
+/* Start Includes */
 #include <RFM69.h>    
 #include <SPI.h>
 #include <SPIFlash.h>
+#include <LowPower.h>
 #include <avr/sleep.h>
 #include <avr/delay.h>
 #include <DHT.h>
 
+/* End Includes */
+
+/* Start Definitions */
 
 #define DHTPIN        7
 #define DHTTYPE       DHT11
 #define NODEID        2    //unique for each node on same network
 #define SENSORID      10   //unique for each sensor
 #define NETWORKID     100  //the same on all nodes that talk to each other
-#define GATEWAYID     1
+#define GATEWAYID     1    //RFM Gateway node id
 #define FREQUENCY     RF69_915MHZ
 #define ENCRYPTKEY    "xxxxxxxxxxxxxxxx" //exactly the same 16 characters/bytes on all nodes!
 #define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
 #define LED           9 // Moteinos have LEDs on D9
 #define FLASH_SS      8 // and FLASH SS on D8
-
 #define SERIAL_BAUD   115200
+
+/* End Definitions */
+
+/* Start Variables */
 
 int TRANSMITPERIOD = 30000; //transmit a packet to gateway so often (in ms)
 boolean requestACK = true;
@@ -37,20 +44,24 @@ typedef struct {
   int            nodeId; //store this nodeId
   int            sensorID;
   unsigned long  var1_usl; //uptime in ms
-  float          var2_float; //from DHT11
-  float          var3_float; //from DHT11
+  float          var2_float; //temp from DHT11
+  float          var3_float; //humidity from DHT11
 } Payload;
 Payload theData;
+
+/* End Variables */
+
+/* Start Setup */
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   dht.begin();
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW
-  radio.setHighPower(); //uncomment only for RFM69HW!
-#endif
+  #ifdef IS_RFM69HW
+    radio.setHighPower(); //uncomment only for RFM69HW!
+  #endif
   radio.encrypt(ENCRYPTKEY);
-  //radio.setFrequency(919000000); //set frequency to some custom frequency
+  radio.sleep();
   char buff[50];
   sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
   Serial.println(buff);
@@ -64,82 +75,24 @@ void setup() {
       Serial.print(flash.UNIQUEID[i], HEX);
       Serial.print(' ');
     }
-    Serial.println();
+    flash.sleep();
   }
   else
     Serial.println("SPI Flash Init FAIL! (is chip present?)");
 }
-
 long lastPeriod = -1;
-void loop() {
-  //process any serial input
-  if (Serial.available() > 0)
-  {
-    char input = Serial.read();
-    if (input >= 48 && input <= 57) //[0,9]
-    {
-      TRANSMITPERIOD = 100 * (input-48);
-      if (TRANSMITPERIOD == 0) TRANSMITPERIOD = 1000;
-      Serial.print("\nChanging delay to ");
-      Serial.print(TRANSMITPERIOD);
-      Serial.println("ms\n");
-    }
 
-    if (input == 'r') //d=dump register values
-      radio.readAllRegs();
-    //if (input == 'E') //E=enable encryption
-    //  radio.encrypt(KEY);
-    //if (input == 'e') //e=disable encryption
-    //  radio.encrypt(null);
+/* End Setup */
 
-    if (input == 'd') //d=dump flash area
-    {
-      Serial.println("Flash content:");
-      int counter = 0;
+/* Start Main Loop */
 
-      Serial.print("0-256: ");
-      while(counter<=256){
-        Serial.print(flash.readByte(counter++), HEX);
-        Serial.print('.');
-      }
-      while(flash.busy());
-      Serial.println();
-    }
-    if (input == 'e')
-    {
-      Serial.print("Erasing Flash chip ... ");
-      flash.chipErase();
-      while(flash.busy());
-      Serial.println("DONE");
-    }
-    if (input == 'i')
-    {
-      Serial.print("DeviceID: ");
-      word jedecid = flash.readDeviceId();
-      Serial.println(jedecid, HEX);
-    }
-  }
+void loop() {   
 
-  //check for any received packets
-  if (radio.receiveDone())
-  {
-    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
-    for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]);
-    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
-
-    if (radio.ACKRequested())
-    {
-      radio.sendACK();
-      Serial.print(" - ACK sent");
-    }
-    Blink(LED,3);
-    Serial.println();
-  }
-
-  int currPeriod = millis()/TRANSMITPERIOD;
-  if (currPeriod != lastPeriod)
-  {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  
+  //int currPeriod = millis()/TRANSMITPERIOD;
+  //if (currPeriod != lastPeriod)
+  //{
     //fill in the struct with new values
     theData.nodeId = NODEID;
     theData.sensorID = SENSORID;
@@ -150,13 +103,21 @@ void loop() {
     Serial.print(sizeof(theData));
     Serial.print(" bytes) ... ");
     if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData)))
+    {
       Serial.print(" ok!");
+      radio.sleep();
+    }
     else Serial.print(" nothing...");
     Serial.println();
     Blink(LED,3);
-    lastPeriod=currPeriod;
-  }
-}
+    //lastPeriod=currPeriod;
+  //}
+} 
+
+/* End Main Loop */
+
+/* Start Blink Method */
+// Blinks LED
 
 void Blink(byte PIN, int DELAY_MS)
 {
@@ -166,30 +127,5 @@ void Blink(byte PIN, int DELAY_MS)
   digitalWrite(PIN,LOW);
 }
 
-void checkDHT()
-{
-  //delay(2000);
-  
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
-  
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from sensor");
-    return;
-  }
-  
-  float hi = dht.computeHeatIndex(f, h);
-  
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print("%t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print("*F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hi);
-  Serial.println(" *F");
-}
+/* End Blink Method */
+
